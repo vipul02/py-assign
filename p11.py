0510f463297a3ba232c60408b1d9921a8afd63f0
vipul02 <vipul.2.july@gmail.com>
2020-01-21 17:09:21 +0530

    p11 completed

--- p11.py
+++ p11.py
@@ -1,1 +1,23 @@
 from bs4 import BeautifulSoup
+import requests
+
+url = 'https://jenkins.io/pipeline/getting-started-pipelines/'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# removes a tag or string from the tree. It returns the tag or string that was extracted, this will remove all style and script
+for tag in soup(["script", "style"]):
+    print(tag.extract()) 
+
+# get text
+text = soup.get_text()
+print(text.splitlines())
+
+# break into lines and remove leading and trailing space on each
+lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+text = '\n'.join(chunk for chunk in chunks if chunk)
+
+print(text)
